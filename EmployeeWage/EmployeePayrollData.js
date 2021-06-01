4fd173f69027c69b4ec4e7182d0edcfb38a6e7d6
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-06-01 23:24:22 +0530

    Added:To validate the name using Regex Pattern

--- EmployeeWage/EmployeePayrollData.js
+++ EmployeeWage/EmployeePayrollData.js
@@ -1,13 +1,12 @@
-console.log("UC-12");
+console.log("UC-13");
 //To create Employee Payroll Class
 
 class EmployeePayrollData {
      id;
-     name;
      salary;
      gender;
      startDate;
-    
+
      constructor(...params) {
        this.id = params[0];
        this.name = params[1];
@@ -17,7 +16,12 @@ class EmployeePayrollData {
      }
 
      get name() {return this._name;}
-     set name(name) {this._name = name;}
+     set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if(nameRegex.test(name))
+           this._name = name;
+        else throw 'Name is Incorrect!';
+     }
 
      toString() {
         const options = {year: 'numeric' , month: 'long', day: 'numeric'};
@@ -28,7 +32,11 @@ class EmployeePayrollData {
 }
      const employeePayrolldata = new EmployeePayrollData(1, "Mark", 30000);
      console.log("Payroll Data: "+employeePayrolldata.toString());
-     employeePayrolldata.name = "James";
+     try{
+     employeePayrolldata.name = 'john';
      console.log("After Replacement: "+employeePayrolldata.toString());
+     } catch (e) {
+       console.error(e);
+     }
      const newemployeePayrolldata = new EmployeePayrollData(1, "Terrisa", 60000 ,"F", "2020-05-31");
      console.log("New Payroll Data: "+newemployeePayrolldata.toString());
